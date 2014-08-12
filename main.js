124dd704a4d4ab6e21ae31a3f945d413fb1530da
Christopher Reed <mindspread@gmail.com>
2014-08-12 14:06:09 -0400

    Inital Commit

---
+++ main.js
@@ -0,0 +1,14 @@
+/*!
+ * Brackets Dark Status Bar
+ *
+ * @author Chris Reed
+ * @license http://opensource.org/licenses/MIT
+ */
+
+define(function (require, exports, module) {
+    // Brackets modules
+    var ExtensionUtils  = brackets.getModule("utils/ExtensionUtils");
+    // Load CSS
+    ExtensionUtils.loadStyleSheet(module, "css/darkStatus.css");
+
+});
